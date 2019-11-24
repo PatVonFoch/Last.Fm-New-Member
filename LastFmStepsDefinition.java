c38d12eb8c4fa16a21123317c9ed57153a1e39a1
PatVF <54185973+PatVonFoch@users.noreply.github.com>
2019-11-24 12:10:49 +0100

    Update LastFmStepsDefinition.java

--- LastFmStepsDefinition.java
+++ LastFmStepsDefinition.java
@@ -1,5 +1,6 @@
 package Cucumber.steps;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -24,33 +25,33 @@ public class LastFmStepsDefinition {
         //_evidon-accept-button
     }
 
-    @When("^User fills out correctly Username field$")
-    public void userFillsOutCorrectlyUsernameField() {
+    @When("^User fills out correctly Username field with \"([^\"]*)\"$")
+    public void userFillsOutCorrectlyUsernameFieldWith(String arg0) {
         WebElement Username = driver.findElement(By.name("userName"));
         Username.clear();
-        Username.sendKeys("Ninogniew");
-        //element.submit();
+        Username.sendKeys(arg0);
+
     }
 
-    @And("^User fills out correctly Email field$")
-    public void userFillsOutCorrectlyEmailField() {
+    @And("^User fills out correctly Email field with \"([^\"]*)\"$")
+    public void userFillsOutCorrectlyEmailFieldWith(String arg1) {
         WebElement Mail = driver.findElement(By.name("email"));
         Mail.clear();
-        Mail.sendKeys("nino@mail.co");
+        Mail.sendKeys(arg1);
     }
 
-    @And("^User fills out correctly Password field$")
-    public void userFillsOutCorrectlyPasswordField() {
+    @And("^User fills out correctly Password field with \"([^\"]*)\"$")
+    public void userFillsOutCorrectlyPasswordFieldWith(String arg2)  {
         WebElement Password = driver.findElement(By.name("password"));
         Password.clear();
-        Password.sendKeys("qwer5%");
+        Password.sendKeys(arg2);
     }
 
-    @And("^User fills out correctly Confirm Password field$")
-    public void userFillsOutCorrectlyConfirmPasswordField() {
+    @And("^User fills out correctly Confirm Password field with \"([^\"]*)\"$")
+    public void userFillsOutCorrectlyConfirmPasswordFieldWith(String arg3){
         WebElement PassConf = driver.findElement(By.name("passwordConf"));
         PassConf.clear();
-        PassConf.sendKeys("qwer5%");
+        PassConf.sendKeys(arg3);
     }
 
     @And("^Accepts terms and tickets i'm not robot$")
@@ -64,14 +65,18 @@ public class LastFmStepsDefinition {
     }
         @Then("^User submits by clicking Create my account$")
         public void userSubmitsByClickingCreateMyAccount() {
-            driver.findElement(By.cssSelector("button.btn-primary")).click();
+            WebElement submitButton = driver.findElement(By.className("form-submit")).findElement(By.tagName("button"));
         }
 
 
         @And("^User becomes a member$")
         public void userBecomesAMember() {
-        }
+        // receives a Verifying email from Last.fm
+            }
+
 
 }
+
+
 
 
